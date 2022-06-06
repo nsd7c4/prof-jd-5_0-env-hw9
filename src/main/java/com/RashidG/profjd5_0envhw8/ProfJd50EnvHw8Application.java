b0728bbd5397e3bf8524e0d4bc71f487e7ea30b3
rashidgafurov <nsd7c4@gmail.com>
2022-06-06 01:58:02 -0400

    Initial commit

---
+++ src/main/java/com/RashidG/profjd5_0envhw8/ProfJd50EnvHw8Application.java
@@ -0,0 +1,13 @@
+package com.RashidG.profjd5_0envhw8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfJd50EnvHw8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfJd50EnvHw8Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/RashidG/profjd5_0envhw8/ProfJd50EnvHw8ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RashidG.profjd5_0envhw8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfJd50EnvHw8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
